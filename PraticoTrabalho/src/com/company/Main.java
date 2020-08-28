7a6b016952fa0554ade02d121d0671b61088e320
lucas <lucasgabrielleitesilva@gmail.com>
2020-08-28 11:54:05 -0300

    Lucas alteração

--- PraticoTrabalho/src/com/company/Main.java
+++ PraticoTrabalho/src/com/company/Main.java
@@ -15,14 +15,56 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
 //    Listas
-
+    public static ArrayList<Aluno> alunos = new ArrayList<>();
+    public static ArrayList<Exemplar> exemplares = new ArrayList<>();
+    public static ArrayList<Funcionario> funcionarios = new ArrayList<>();            
+    public static ArrayList<Professor> professores = new ArrayList<>();
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Controller controller = new Controller();
+        
+        String opcao;
+        int op =-1;
+        
+        while(op != 0){
+            System.out.println("--- TRABALHO PRÁTICO FINAL - LINGUAGEM DE PROGRAMAÇÃO II ---");
+            System.out.println("1 - Cadastrar\n");
+            System.out.println("2 - Excluir\n");
+            op = scanner.nextInt();
+            
+            switch(op){
+                case 1:{
 
+                    System.out.println("---Olá, que bom que deseja se cadastrar!---\n");
+                    System.out.println("Para se cadstarar nas classes escolha uma letra referente a classe desejada:");
+                    System.out.println("[A] => Aluno\n[E] => Exemplar\n[F] => Funcionario\n[P] => Professor");
+                    opcao = scanner.next();
+                    opcao = opcao.toUpperCase();
+                    
+                    controller.novoCadastro(opcao);
+                    
+                    break;
+                }
+                
+                case 2:{
+                    
+                    System.out.println("---Olá, vamos excluir!---\n");
+                    System.out.println("Para excluir nas classes escolha uma letra referente a classe desejada:");
+                    System.out.println("[A] => Aluno\n[E] => Exemplar\n[F] => Funcionario\n[P] => Professor");
+                    opcao = scanner.next();
+                    opcao = opcao.toUpperCase();
+                    
+                    controller.excluiNaLista(opcao);
+                    break;
+                }
+            }
+        }
 
 
     }
