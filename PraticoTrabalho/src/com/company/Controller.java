ddf31ae1e5d7a467b42099f0210d9ecd799386d2
lucas <lucasgabrielleitesilva@gmail.com>
2020-08-31 00:17:04 -0300

    Novas atualizações 00:16

--- PraticoTrabalho/src/com/company/Controller.java
+++ PraticoTrabalho/src/com/company/Controller.java
@@ -4,12 +4,17 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Controller {
 
     private ArrayList<Pessoa> listaPessoas;
     private ArrayList<Emprestimo> listaEmprestimo;
     private ArrayList<Exemplar> listaExemplar;
+    
+    Scanner scanner = new Scanner(System.in);
+    boolean flag = false;
+    int i = 0;
 
     public Controller() {
         this.listaPessoas = new ArrayList();
@@ -40,11 +45,18 @@ public class Controller {
     public void setListaExemplar(ArrayList<Exemplar> listaExemplar) {
         this.listaExemplar = listaExemplar;
     }
+    
+    public void inserePessoa(Pessoa pessoa){
+        this.listaPessoas.add(pessoa);
+    }
+    
+    public void insereExemplar(Exemplar exemplar){
+        this.listaExemplar.add(exemplar);
+    }
 
     public void leitorPessoas (){//le um arquivo com algumas pessoas, já com valores de seus atributos
-        Pessoa pessoa = new Pessoa() {};
-        
-        String nomeArq = "Entrada.txt";
+              
+        String nomeArq = "Pessoa.txt";
         int nL;
         
         try {
@@ -59,19 +71,16 @@ public class Controller {
             int idade;
             String cpf;
             
-            for (int i = 0; i < nL; i++) {
+            for (i = 0; i < nL; i++) {
                 linha = lerArq.readLine();
                 informacao = linha.split(" ");//Para separar as informações
                 nome = informacao[0];
                 idade = Integer.parseInt(informacao[1]);
                 cpf = informacao[2];
-                
-                pessoa.setNome(nome);
-                pessoa.setIdade(idade);
-                pessoa.setCPF(cpf);
-                
-                this.listaPessoas.add(pessoa);
+
+                this.inserePessoa(new Pessoa(nome, idade, cpf) {});
             }
+            System.out.println("\nLista adicionada!\n");
             
         } catch (IOException e) {
             System.out.println("Erro ao ler o arquivo!");
@@ -79,8 +88,7 @@ public class Controller {
     }
     
         public void leitorExemplares (){// le um arquivo com alguns exemplares, já com valores de seus atributos
-        Exemplar exemplar = new Exemplar();
-        
+
         String nomeArq = "Exemplares.txt";
         int nL;
         
@@ -96,23 +104,80 @@ public class Controller {
             String isbn;
             int cod;
             
-            
-            for (int i = 0; i < nL; i++) {
+            for (i = 0; i < nL; i++) {
                 linha = lerArq.readLine();
-                informacao = linha.split(",");//Para separar as informações
+                informacao = linha.split(", ");//Para separar as informações
                 nome = informacao[0];
                 isbn = informacao[1];
                 cod = Integer.parseInt(informacao[2]);
-                
-                exemplar.setNome(nome);
-                exemplar.setISBN(isbn);
-                exemplar.setCod(cod);
-                
-                this.listaExemplar.add(exemplar);
+                           
+                this.insereExemplar(new Exemplar(nome, isbn, cod));
             }
+            System.out.println("\nLista adicionada!\n");
             
         } catch (IOException e) {
             System.out.println("Erro ao ler o arquivo!");
+        }
+    }
+        
+    public void criaPessoa(){//Solicitado para criar uma pessoa fora da lista txt
+        
+        String nome;
+        int idade;
+        String cpf;
+        
+        System.out.println("\n*** Nova Pessoa ***");
+        System.out.println("\n Informe o nome:"); nome = scanner.next();
+        System.out.println("\n Informe a idade: "); idade = scanner.nextInt();
+        System.out.println("\n Informe o CPF: "); cpf = scanner.next();
+        System.out.println("\n*********************\n");
+        
+        this.inserePessoa(new Pessoa(nome, idade, cpf) {});
+        System.out.println("\nPessoa criada!");
+        System.out.println("\n*********************\n");
+    }
+    
+    public void criaExemplar(){//Solicitado para criar um exemplar fora da lista txt
+        
+        String nome;
+        String isbn;
+        int cod;
+        
+        System.out.println("\n*** Novo Exemplar ***");
+        System.out.println("\n Informe o nome:"); nome = scanner.next();
+        System.out.println("\n Informe o codigo: "); cod = scanner.nextInt();
+        System.out.println("\n Informe o ISBN: "); isbn = scanner.next();
+                
+        this.insereExemplar(new Exemplar(nome, isbn, cod));
+        System.out.println("\nExemplar criado!");
+        System.out.println("\n*********************\n");
+    }
+    
+    public void exclusaoPessoa(){
+        String cpf; 
+        
+        System.out.println("\n*** Remover uma pessoa da lista ***");
+        System.out.println("\n Informe o CPF da pessoa que deseja remover da lista: "); cpf = scanner.next();
+        
+        for ( i = 0; i < this.listaPessoas.size(); i++) {
+            if(this.listaPessoas.get(i).getCPF().equals(cpf)){
+                this.listaPessoas.remove(i);
+                System.out.println("\nRemoção cocluida!\n");
+            }
+        }
+    }
+    
+    public void exclusaoExemplar(){
+        String isbn; 
+        
+        System.out.println("\n*** Remover uma pessoa da lista ***");
+        System.out.println("\n Informe o ISBN do exemplar que deseja remover da lista: "); isbn = scanner.next();
+        
+        for ( i = 0; i < this.listaExemplar.size(); i++) {
+            if(this.listaExemplar.get(i).getISBN().equals(isbn)){
+                this.listaExemplar.remove(i);
+                System.out.println("\nRemoção cocluida!\n");
+            }
         }
     }
 }
--- PraticoTrabalho/src/com/company/Main.java
+++ PraticoTrabalho/src/com/company/Main.java
@@ -29,44 +29,78 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Controller controller = new Controller();
         
-        String opcao;
         int op =-1;
-        
+       
+        System.out.println("--- TRABALHO PRÁTICO FINAL - LINGUAGEM DE PROGRAMAÇÃO II ---");
         while(op != 0){
-            System.out.println("--- TRABALHO PRÁTICO FINAL - LINGUAGEM DE PROGRAMAÇÃO II ---");
-            System.out.println("1 - Cadastrar\n");
-            System.out.println("2 - Excluir\n");
+            System.out.println("\n------------------------------------------\n");
+            System.out.println("1 - Cadastrar lista pronta (PESSOAS)\n");
+            System.out.println("2 - Cadastrar lista pronta (EXEMPLARES)\n");
+            System.out.println("3 - Criar uma nova Pessoa\n");
+            System.out.println("4 - Criar um novo Exemplar\n");
+            System.out.println("5 - Mostar lista de Pessoas\n");
+            System.out.println("6 - Mostar lista de Exemplares\n");
+            System.out.println("7 - Remover uma Pessoa\n");
+            System.out.println("8 - Remover um Exemplar\n");
+            System.out.println("0 - Encerrar\n");
             op = scanner.nextInt();
             
             switch(op){
-//                case 1:{
-//
-//                    System.out.println("---Olá, que bom que deseja se cadastrar!---\n");
-//                    System.out.println("Para se cadstarar nas classes escolha uma letra referente a classe desejada:");
-//                    System.out.println("[A] => Aluno\n[E] => Exemplar\n[F] => Funcionario\n[P] => Professor");
-//                    opcao = scanner.next();
-//                    opcao = opcao.toUpperCase();
-//                    
-//                    
-//                    
-//                    break;
-//                }
-//                
-//                case 2:{
-//                    
-//                    System.out.println("---Olá, vamos excluir!---\n");
-//                    System.out.println("Para excluir nas classes escolha uma letra referente a classe desejada:");
-//                    System.out.println("[A] => Aluno\n[E] => Exemplar\n[F] => Funcionario\n[P] => Professor");
-//                    opcao = scanner.next();
-//                    opcao = opcao.toUpperCase();
-//                    
-//                    
-//                    break;
-//                }
+                case 1:{
+                    controller.leitorPessoas();
+                    break;
+                }
+                
+                case 2:{
+                    controller.leitorExemplares();       
+                    break;
+                }
+                
+                case 3:{
+                    controller.criaPessoa();
+                    break;
+                }
+                
+                case 4:{
+                    controller.criaExemplar();
+                    break;
+                }
+                
+                case 5:{
+                    
+                    for (int i = 0; i < controller.getListaPessoas().size(); i++) {
+                        System.out.println("\n-------------------");
+                        System.out.println("\nNome:" + controller.getListaPessoas().get(i).getNome());
+                        System.out.println("\nCPF:"+ controller.getListaPessoas().get(i).getCPF());
+                        System.out.println("\nIdade:" + controller.getListaPessoas().get(i).getIdade());
+                    }
+                    
+                    break;
+                }
+                
+                case 6:{
+                    
+                    for (int i = 0; i < controller.getListaExemplar().size(); i++) {
+                        System.out.println("\n-------------------");
+                        System.out.println("\nNome:" + controller.getListaExemplar().get(i).getNome());
+                        System.out.println("\nIsbn:"+ controller.getListaExemplar().get(i).getISBN());
+                        System.out.println("\ncod:" + controller.getListaExemplar().get(i).getCod());
+                    }
+                    
+                    break;
+                }
+                
+                case 7: {
+                    controller.exclusaoPessoa();
+                    break;
+                }
+                
+                case 8: {
+                    controller.exclusaoExemplar();
+                    break;
+                }
             }
         }
-
-
     }
 }
 
