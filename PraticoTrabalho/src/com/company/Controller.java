20d1c702d25f096421037fe44522319206964dfd
lucas <lucasgabrielleitesilva@gmail.com>
2020-08-30 21:53:42 -0300

    Leitor de arquivos.txt

--- PraticoTrabalho/src/com/company/Controller.java
+++ PraticoTrabalho/src/com/company/Controller.java
@@ -41,4 +41,78 @@ public class Controller {
         this.listaExemplar = listaExemplar;
     }
 
+    public void leitorPessoas (){//le um arquivo com algumas pessoas, já com valores de seus atributos
+        Pessoa pessoa = new Pessoa() {};
+        
+        String nomeArq = "Entrada.txt";
+        int nL;
+        
+        try {
+            FileReader arq = new FileReader(nomeArq);
+            BufferedReader lerArq = new BufferedReader(arq);
+            
+            String linha= lerArq.readLine(); //Vai ler a primeira linha do TXT, que no caso contém o numero de usuarios.
+            nL = Integer.parseInt(linha);
+            
+            String informacao[];
+            String nome;
+            int idade;
+            String cpf;
+            
+            for (int i = 0; i < nL; i++) {
+                linha = lerArq.readLine();
+                informacao = linha.split(" ");//Para separar as informações
+                nome = informacao[0];
+                idade = Integer.parseInt(informacao[1]);
+                cpf = informacao[2];
+                
+                pessoa.setNome(nome);
+                pessoa.setIdade(idade);
+                pessoa.setCPF(cpf);
+                
+                this.listaPessoas.add(pessoa);
+            }
+            
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo!");
+        }
+    }
+    
+        public void leitorExemplares (){// le um arquivo com alguns exemplares, já com valores de seus atributos
+        Exemplar exemplar = new Exemplar();
+        
+        String nomeArq = "Exemplares.txt";
+        int nL;
+        
+        try {
+            FileReader arq = new FileReader(nomeArq);
+            BufferedReader lerArq = new BufferedReader(arq);
+            
+            String linha= lerArq.readLine(); //Vai ler a primeira linha do TXT, que no caso contém o numero de usuarios.
+            nL = Integer.parseInt(linha);
+            
+            String informacao[];
+            String nome;
+            String isbn;
+            int cod;
+            
+            
+            for (int i = 0; i < nL; i++) {
+                linha = lerArq.readLine();
+                informacao = linha.split(",");//Para separar as informações
+                nome = informacao[0];
+                isbn = informacao[1];
+                cod = Integer.parseInt(informacao[2]);
+                
+                exemplar.setNome(nome);
+                exemplar.setISBN(isbn);
+                exemplar.setCod(cod);
+                
+                this.listaExemplar.add(exemplar);
+            }
+            
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo!");
+        }
+    }
 }
